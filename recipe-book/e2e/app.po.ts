85a8880c54f8c23f2b2a9e0b55afeb11040b71c1
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-24 07:56:32 -0400

    initial commit

---
+++ recipe-book/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor';
+
+export class RecipeBookPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('jenny-root h1')).getText();
+  }
+}

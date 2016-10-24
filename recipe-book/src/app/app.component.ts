72634143df593aac3615c08330ec04a1a8e61f9f
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-24 08:23:40 -0400

    feat: created recipe component
    two column layout that will show recipes

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
+import { RecipesComponent } from './recipes/recipes.component'
 
 @Component({
   selector: 'jenny-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [HeaderComponent, RecipesComponent]
 })
 export class AppComponent {
   title = 'jenny works!';

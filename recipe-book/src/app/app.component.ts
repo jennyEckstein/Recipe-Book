b8e73c2d70011289489c84affe1b255f6bc0e554
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-24 11:18:25 -0400

    feat: added skeleton for adding recipe

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component'
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @Component({
   selector: 'jenny-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'jenny works!';
---
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { ShoppingListAddComponent } from './shopping-list-add.component';
+
+@Component({
+  selector: 'jenny-shopping-list',
+  templateUrl: './shopping-list.component.html',
+})
+export class ShoppingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

e8137d99103ed46894bb607dc814b2fba0784ef8
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-10-27 20:30:11 -0400

    feat: now recipes are injected from service

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,40 +2,24 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe';
 import { RecipeItemComponent } from './recipe-item.component';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'jenny-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-	recipes: Recipe[] = [
-      new Recipe(
-        'Chicken Soup',
-         'Chicken soup is a soup made from chicken, simmered in water, usually with various other ingredients.',
-         'https://recipeland.com/images/r/18707/d8a75a658379a5c91f80_400.jpg',
-         []
-        ),
-      new Recipe(
-         'Meatball',
-         'A meatball is ground or minced meat rolled into a small ball, sometimes along with other ingredients, such as bread crumbs, minced onion, eggs, butter and seasoning.',
-         'http://www.fusionlifebrands.com/sites/default/files/recipe_images/Grandma-Meatballs.jpg',
-         []
-        ),
-      new Recipe(
-          'Lasagne',
-          'Lasagne are wide, flat-shaped pasta, and possibly one of the oldest types of pasta.',
-          'http://www.coolhomerecipes.com/wp-content/uploads/sites/317/2015/04/bechamel-beef-lasagna-400x300.jpg',
-          []
-        )
-  ];
+	recipes: Recipe[] = [];
 	@Output() recipeSelected = new EventEmitter<Recipe>();
 /*	recipe = new Recipe('Dummy', 'Dummy', 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTS29tEchi1J_L0uuONTOItXwi5fylpbpc5dhAzvXDfaDjQ7Ysh');
 */
 
 
-  constructor() { }
+  constructor(
+    private  recipeService: RecipeService) { }
 
   ngOnInit() {
+      this.recipes = this.recipeService.getRecipes();
   }
   onSelected(recipe: Recipe){
   		this.recipeSelected.emit(recipe);
---
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe';
+
+@Injectable()
+export class RecipeService {
+	private recipes: Recipe[] = [
+      new Recipe(
+        'Chicken Soup',
+         'Chicken soup is a soup made from chicken, simmered in water, usually with various other ingredients.',
+         'https://recipeland.com/images/r/18707/d8a75a658379a5c91f80_400.jpg',
+         []
+        ),
+      new Recipe(
+         'Meatball',
+         'A meatball is ground or minced meat rolled into a small ball, sometimes along with other ingredients, such as bread crumbs, minced onion, eggs, butter and seasoning.',
+         'http://www.fusionlifebrands.com/sites/default/files/recipe_images/Grandma-Meatballs.jpg',
+         []
+        ),
+      new Recipe(
+          'Lasagne',
+          'Lasagne are wide, flat-shaped pasta, and possibly one of the oldest types of pasta.',
+          'http://www.coolhomerecipes.com/wp-content/uploads/sites/317/2015/04/bechamel-beef-lasagna-400x300.jpg',
+          []
+        )
+  ];
+
+  constructor() { }
+
+  getRecipes(){
+  	return this.recipes;
+  }
+
+}

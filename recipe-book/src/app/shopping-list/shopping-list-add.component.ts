1ca17a5c43a54e5ffb45d3157dfd306bdd9b98c5
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-13 11:05:30 -0500

    feat: editing functionality for ingredients

--- recipe-book/src/app/shopping-list/shopping-list-add.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 import { Ingredient } from '../shared/ingredient';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -6,20 +6,28 @@ import { ShoppingListService } from './shopping-list.service';
   selector: 'jenny-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
-	items: Ingredient[] = [];
+export class ShoppingListAddComponent implements OnChanges {
+	//items: Ingredient[] = [];
 	isAdd = true;
-	item: Ingredient;
+	@Input() item: Ingredient;
 
   constructor(private sls: ShoppingListService) { }
 
-  ngOnInit() {
-  }
-  onSubmit(ingredient: Ingredient){
-  	if (!this.isAdd){
-  		//edit
+  ngOnChanges(changes){
+  	if(changes.item.currentValue === null){
+  		this.isAdd = true;
+  		this.item = {name: null, amount: null};
   	}else{
-  		this.item = new Ingredient(ingredient.name, ingredient.amount);
+  		this.isAdd = false;
+  	}
+  }
+
+  onSubmit(ingredient: Ingredient){
+  	const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
+  	if (!this.isAdd){
+  		this.sls.editItem(this.item, newIngredient);
+  	}else{
+  		this.item = newIngredient;
   		this.sls.addItem(this.item);
   	}
   }

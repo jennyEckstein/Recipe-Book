7c33ebf25fe4b2815b01b4456032aa94540ecc7a
Jenny Eckstein <jenny.j.eckstein@gmail.com>
2016-11-14 21:53:50 -0500

    feat: storing recipes in Firebase

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 import { Recipe } from './recipe';
 import { Ingredient } from '../shared/ingredient';
 
@@ -25,7 +26,7 @@ export class RecipeService {
         )
   ];
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   getRecipes(){
   	return this.recipes;
@@ -45,6 +46,19 @@ export class RecipeService {
 
   editRecipe(oldRecipe: Recipe, newRecipe: Recipe){
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData(){
+    const body = JSON.stringify(this.recipes);
+    const headers = new Headers({
+        'Content-Type': 'application/json'
+    });
+    return this.http.post('https://recipebook-9db86.firebaseio.com/recipes.json', body, {headers: headers});
+
+  }
+
+  fetchData(){
+
   }
 
 }
